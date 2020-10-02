2978a2a354beb72deee6219b0e3b940308647c66
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2020-10-02 09:26:33 -0700

    Added Player Movement

---
+++ Assets/Scripts/Game Pieces/Ball/BallColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallColor : MonoBehaviour
+{
+    //Renderer m_Renderer;
+
+    void Start()
+    {
+        /*
+        Color color = new Color(0.2f, 0.3f, 0.4f);
+        m_Renderer = GetComponent<Renderer>();
+        transform.renderer.material.color = color;
+        */
+    }
+
+    
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    //Player Speed
+    public float playerSpeed = 10f;
+
+    //Rotation
+    public float rotateSpeed = 180f;
+    private float currentAngle;
+
+    void Update()
+    {
+        //Calculates the angle in terms of pi
+        currentAngle = transform.rotation.y * Mathf.PI / 180;
+
+        //Movement Inputs
+        if (Input.GetKey("left")) transform.Rotate(0f, -rotateSpeed * Time.deltaTime, 0f);
+        if (Input.GetKey("right")) transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f);
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(Mathf.Sin(currentAngle) * Time.deltaTime * playerSpeed, 0f, Mathf.Cos(currentAngle) * Time.deltaTime * playerSpeed);
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(-Mathf.Sin(currentAngle) * Time.deltaTime * playerSpeed, 0f, -Mathf.Cos(currentAngle) * Time.deltaTime * playerSpeed);
+        }
+    }
+
+   
+
+}
