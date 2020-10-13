0ee4d8d643dd85e2615d91febf18c8d9846913a3
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2020-10-13 09:56:44 -0700

    Main Menu + Pause Screens
    Working on Main menu, pause screens, and achievement lists

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
     public bool isStart;
-    //public bool isConrol;
+    public bool isConrol;
     public bool isQuit;
-    
+
     void OnMouseUp()
     {
         if (isStart)
         {
+            Application.LoadLevel(2);
+            GetComponent<Renderer>().material.color = Color.cyan;
+        }
+        if (isQuit) Application.Quit();
+        if (isConrol)
+        {
             Application.LoadLevel(1);
             GetComponent<Renderer>().material.color = Color.cyan;
         }
-    	if (isQuit) Application.Quit();
-        //if (isConrol) Application.LoadLevel(2);
     }
 }
