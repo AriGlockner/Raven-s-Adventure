6cbb987323adb988e4389ec3c9ec9c391d3a0cb7
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2020-10-16 09:37:27 -0700

    Main Menu
    Main menu is done except for switching between scenes.

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -5,21 +5,42 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
     public bool isStart;
-    public bool isConrol;
+    public bool isControl;
     public bool isQuit;
 
-    void OnMouseUp()
-    {
-        if (isStart)
-        {
-            Application.LoadLevel(2);
-            GetComponent<Renderer>().material.color = Color.cyan;
-        }
-        if (isQuit) Application.Quit();
-        if (isConrol)
-        {
-            Application.LoadLevel(1);
-            GetComponent<Renderer>().material.color = Color.cyan;
-        }
-    }
+	void Start()
+	{
+		GetComponent<Renderer>().material.color = Color.black;
+	}
+	
+	void OnMouseEnter()
+	{
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+	
+	void OnMouseExit()
+	{
+		if (isStart) start();
+		if (isControl) control();
+		if (isQuit) quit();
+		
+	}
+	
+	void start()
+	{
+		Debug.Log("Start");
+		GetComponent<Renderer>().material.color = Color.black;
+	}
+	
+	void control()
+	{
+		Debug.Log("Controls");
+		GetComponent<Renderer>().material.color = Color.black;
+	}
+	
+	void quit()
+	{
+		Debug.Log("Quit");
+		GetComponent<Renderer>().material.color = Color.black;
+	}
 }
---
+++ Assets/Scripts/Main Menu/SwitchScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    public int goToScene;
+
+    void Start()
+    {
+        Debug.Log("Start!");
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("Load Scene: " + goToScene);
+        SceneManager.LoadScene(sceneBuildIndex:goToScene);
+    }
+}
